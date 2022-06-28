d725340b5cfc4bf7dcf9cc26b664010408fea410
Smruti Swajalika <s6swajalika@gmail.com>
2022-06-28 14:57:30 +0530

    Spelling corrected

--- server/models/db.js
+++ server/models/db.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI, (err) => {
-    if(!err) { console.log('MomgoDB connection suceeded.'); }
-    else { console.log('Error in MomgoDB connection:'+ JSON.stringify(err,undefined,2)); }
+    if(!err) { console.log('MongoDB connection suceeded.'); }
+    else { console.log('Error in MongoDB connection:'+ JSON.stringify(err,undefined,2)); }
 });
 
 require('./user.model');
